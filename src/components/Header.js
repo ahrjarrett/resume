57fc5035a22ba80b823b510fdeccbbb75c89b15f
Andrew Jarrett <ahrjarrett@gmail.com>
2020-03-19 12:12:12 -0500

    update title @ top

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,38 @@
 import React from 'react'
 
 const Header = () => (
-  <div className="Header">
-    <div className="header-left">
-      <h1 className="full-name">Andrew Jarrett</h1>
-      <p className="job-title">Software Engineer, Data Viz Nerd 📈</p>
-      <p className="mini-bio" />
+  <div className='Header'>
+    <div className='header-left'>
+      <h1 className='full-name'>Andrew Jarrett</h1>
+      <p className='job-title'>React Engineer, Data Viz Nerd</p>
+      <p className='mini-bio' />
     </div>
-    <nav className="nav-contact">
-      <a className="external-link" href="mailto:ahrjarrett@gmail.com">
+    <nav className='nav-contact'>
+      <a className='external-link' href='mailto:ahrjarrett@gmail.com'>
         ahrjarrett@gmail.com
-        <img alt="" className="resume-icon" src="svg/mail.svg" />
+        <img alt='' className='resume-icon' src='svg/mail.svg' />
       </a>
       <a
-        className="external-link"
-        href="https://www.linkedin.com/in/ahrjarrett/"
-        target="_blank"
-        rel="noopener noreferrer"
+        className='external-link'
+        href='https://www.linkedin.com/in/ahrjarrett/'
+        target='_blank'
+        rel='noopener noreferrer'
       >
         linkedin.com/in/ahrjarrett
-        <img alt="" className="resume-icon" src="svg/linkedin.svg" />
+        <img alt='' className='resume-icon' src='svg/linkedin.svg' />
       </a>
       <a
-        className="external-link"
-        href="https://github.com/ahrjarrett"
-        target="_blank"
-        rel="noopener noreferrer"
+        className='external-link'
+        href='https://github.com/ahrjarrett'
+        target='_blank'
+        rel='noopener noreferrer'
       >
         github.com/ahrjarrett
-        <img alt="" className="resume-icon" src="svg/github.svg" />
+        <img alt='' className='resume-icon' src='svg/github.svg' />
       </a>
-      <span className="italics">
+      <span className='italics'>
         Austin, Texas
-        <img alt="" className="resume-icon" src="svg/location.svg" />
+        <img alt='' className='resume-icon' src='svg/location.svg' />
       </span>
     </nav>
   </div>
